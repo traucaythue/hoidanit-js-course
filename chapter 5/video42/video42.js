7c7542a1a7d678748cd0b05875f55cfc76ef8d99
traucaythue <traucaythue.vn@gmail.com>
2026-05-27 09:08:19 +0700

    update video42

---
+++ chapter 5/video42/video42.js
@@ -0,0 +1,32 @@
+console.log("video42");
+
+// Khai báo scores array
+const scores = [10, 4, 7, 2, 5];
+
+// Khai báo object
+
+const sinhVien1 = {
+  name: "hoidanit",
+  score: 7,
+  class: 12,
+  address: "ha noi",
+};
+
+console.log(sinhVien1);
+
+const sinhVien2 = {
+  name: "eric",
+  score: 9,
+  class: 10,
+  address: "hai phong",
+};
+
+console.log(sinhVien2);
+
+// Khai báo sinhVien array
+
+const sinhVien = [sinhVien1, sinhVien2];
+
+console.log(">>> score", scores);
+
+console.log(">>> sinh vien ", sinhVien);
---
+++ chapter 5/video42/video42.md
@@ -0,0 +1,15 @@
+## Khái niệm
+
+- Object là kiểu dữ liệu dùng để lưu trữ thông tin có cấu trúc theo cặp thuộc tính (key) và giá trị (value)
+
+## Ví dụ
+
+- Cách khai báo object:
+
+let obj_name = {
+key : value,
+}
+
+## Lưu ý/ Lỗi thường gặp
+
+- cặp key:value luông nằm trong "{}"
